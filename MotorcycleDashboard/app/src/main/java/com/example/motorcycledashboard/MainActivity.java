43d496a42e960a7d039a35c6046d1812102b5e4e
LukeDowdall1 <43385485+LukeDowdall1@users.noreply.github.com>
2022-01-26 12:59:24 +0000

    GPS

--- MotorcycleDashboard/app/src/main/java/com/example/motorcycledashboard/MainActivity.java
+++ MotorcycleDashboard/app/src/main/java/com/example/motorcycledashboard/MainActivity.java
@@ -1,15 +1,120 @@
 package com.example.motorcycledashboard;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SwitchCompat;
 
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.widget.CompoundButton;
+import android.widget.Switch;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.Formatter;
+import java.util.Locale;
+
+public class MainActivity extends AppCompatActivity implements LocationListener {
+
+    Switch switch_metric;
+    TextView speed;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.dashboard);
-        
+
+        switch_metric = findViewById(R.id.switch_metric);
+
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+            requestPermissions(new String[] {Manifest.permission.ACCESS_FINE_LOCATION}, 1000);
+        }
+        else {
+            doStuff();
+        }
+
+        this.updateSpeed(null);
+
+        switch_metric.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                MainActivity.this.updateSpeed(null);
+            }
+        });
+    }
+
+    @Override
+    public void onLocationChanged(@NonNull Location location) {
+        if(location != null) {
+            LocMain myLocation = new LocMain(location, this.useMetricUnits());
+            this.updateSpeed(myLocation);
+        }
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(@NonNull String provider) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(@NonNull String provider) {
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if(grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            doStuff();
+        }
+        else {
+            finish();
+        }
+    }
+
+    @SuppressLint("MissingPermission")
+    private void doStuff() {
+        LocationManager locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+        if (locationManager!= null) {
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,0, 0, this);
+        }
+        Toast.makeText(this, "Connecting GPS...", Toast.LENGTH_SHORT).show();
+    }
+
+    private void updateSpeed(LocMain location) {
+        float nCurrentSpeed = 0;
+
+        if(location != null){
+            location.setMetricUnits(this.useMetricUnits());
+            nCurrentSpeed = location.getSpeed();
+        }
+
+        Formatter fmt = new Formatter(new StringBuilder());
+        fmt.format(Locale.US, "%5.1f", nCurrentSpeed);
+        String strCurrentSpeed = fmt.toString();
+        strCurrentSpeed = strCurrentSpeed.replace(" ", "0");
+
+        if (this.useMetricUnits()) {
+            speed.setText(strCurrentSpeed + "km/h");
+        }
+        else {
+            speed.setText(strCurrentSpeed + "mph");
+        }
+    }
+
+    private boolean useMetricUnits() {
+        return switch_metric.isChecked();
     }
 }
