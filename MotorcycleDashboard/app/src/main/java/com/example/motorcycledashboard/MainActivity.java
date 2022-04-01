08c08d682348e6dd290d0ebba63bbbf23b21800a
LukeDowdall1 <43385485+LukeDowdall1@users.noreply.github.com>
2022-04-01 15:13:56 +0100

    Add Map Pointers

--- MotorcycleDashboard/app/src/main/java/com/example/motorcycledashboard/MainActivity.java
+++ MotorcycleDashboard/app/src/main/java/com/example/motorcycledashboard/MainActivity.java
@@ -9,6 +9,10 @@ import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
@@ -24,34 +28,69 @@ import android.widget.Switch;
 import android.widget.TextView;
 import android.widget.Toast;
 
+
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Formatter;
 import java.util.List;
 import java.util.Locale;
 
-public class MainActivity extends AppCompatActivity implements LocationListener, View.OnClickListener {
+
+public class MainActivity extends AppCompatActivity implements LocationListener, View.OnClickListener, SensorEventListener {
+
+    private static final String TAG = "Main";
+
+    public static ArrayList<LatLng> myObjects = new ArrayList<>();
 
     Switch switch_metric;
     TextView TV_speed;
     TextView TV_TopSpeed;
     TextView TV_AvgSpeed;
     TextView TV_LeanAngle;
+    TextView TV_MaxLeanR;
+    TextView TV_MaxLeanL;
     Button button1;
     Button button2;
-
+    float Azimut;
 
 
     public float TopSpeed;
     public List<Float> AvgSpeedList = new ArrayList<>();
     public String strAverage;
+    public float LeanAngle;
+    public int MaxLeanL = 0;
+    public int MaxLeanR = 0;
     public int Num = 0;
-    
+
+    private SensorManager mSensorManager;
+    private Sensor mAccelerometer;
+    private Sensor mMagnetometer;
+
+    private float[] mLastAccelerometer = new float[3];
+    private float[] mLastMagnetometer = new float[3];
+    private boolean mLastAccelerometerSet = false;
+    private boolean mLastMagnetometerSet = false;
+
+    private float[] mR = new float[9];
+    private float[] mOrientation = new float[3];
+
+    private MarkerOptions options = new MarkerOptions();
+    private ArrayList<LatLng> latlngs = new ArrayList<>();
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        int i = 0;
         super.onCreate(savedInstanceState);
         setContentView(R.layout.dashboard);
+
 
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
 
@@ -59,30 +98,34 @@ public class MainActivity extends AppCompatActivity implements LocationListener,
         TV_TopSpeed = findViewById(R.id.TV_TopSpeed);
         TV_AvgSpeed = findViewById(R.id.TV_AvgSpeed);
         TV_LeanAngle = findViewById(R.id.TV_LeanAngle);
+        TV_MaxLeanL = findViewById(R.id.TV_MaxLeanL);
+        TV_MaxLeanR = findViewById(R.id.TV_MaxLeanR);
         button1 = findViewById(R.id.button1);
         button2 = findViewById(R.id.button2);
         button1.setOnClickListener(this);
         button2.setOnClickListener(this);
 
+        myObjects.add(new LatLng(53.353946, -6.252994));
+        myObjects.add(new LatLng(53.351929, -6.247714));
+
         switch_metric = findViewById(R.id.switch_metric);
 
         TV_TopSpeed.setText("Top speed: " + TopSpeed + " km/h");
+        TV_LeanAngle.setText("Lean Angle: " + LeanAngle + " km/h");
 
 
-        if (strAverage == null){
+        if (strAverage == null) {
             TV_AvgSpeed.setText("Avg. speed: " + "0" + " km/h");
-        }
-        else {
+        } else {
             TV_AvgSpeed.setText("Avg. speed: " + strAverage + " km/h");
         }
 
         Log.i("On", "Create: ");
 
 
-        if(checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            requestPermissions(new String[] {Manifest.permission.ACCESS_FINE_LOCATION}, 1000);
-        }
-        else {
+        if (checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1000);
+        } else {
             doStuff();
         }
 
@@ -96,6 +139,10 @@ public class MainActivity extends AppCompatActivity implements LocationListener,
         });
 
 
+        mSensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+        mAccelerometer = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        mMagnetometer = mSensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD);
+
     }
 
 
@@ -106,17 +153,16 @@ public class MainActivity extends AppCompatActivity implements LocationListener,
             TV_LeanAngle.setText("Num: " + TopSpeed);
         }
         if (v == button2) {
-//            Intent intent = new Intent(MainActivity.this, Map.class);
-//            startActivity(intent);
-//            setContentView(R.layout.mainmap);
+            Intent intent = new Intent(MainActivity.this, Map.class);
+
+            startActivity(intent);
         }
     }
 
 
-
     @Override
     public void onLocationChanged(@NonNull Location location) {
-        if(location != null) {
+        if (location != null) {
             LocMain myLocation = new LocMain(location, this.useMetricUnits());
             this.updateSpeed(myLocation);
         }
@@ -138,22 +184,20 @@ public class MainActivity extends AppCompatActivity implements LocationListener,
     }
 
 
-
     @SuppressLint("MissingPermission")
     private void doStuff() {
         LocationManager locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
-        if (locationManager!= null) {
-            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,0, 0, this);
+        if (locationManager != null) {
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, this);
         }
         Toast.makeText(this, "Connecting GPS...", Toast.LENGTH_SHORT).show();
     }
 
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        if(grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+        if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
             doStuff();
-        }
-        else {
+        } else {
             finish();
         }
     }
@@ -163,12 +207,11 @@ public class MainActivity extends AppCompatActivity implements LocationListener,
         float nCurrentSpeed = 0;
 
 
-        if(location != null){
+        if (location != null) {
             location.setMetricUnits(this.useMetricUnits());
             nCurrentSpeed = location.getSpeed();
             nCurrentSpeed = Math.round(nCurrentSpeed);
         }
-
 
 
         Formatter fmt = new Formatter(new StringBuilder());
@@ -181,7 +224,6 @@ public class MainActivity extends AppCompatActivity implements LocationListener,
         String strTopSpeed = fmt2.toString();
 
 
-
         if (nCurrentSpeed > TopSpeed) {
             strTopSpeed = strCurrentSpeed;
             TopSpeed = nCurrentSpeed;
@@ -191,21 +233,33 @@ public class MainActivity extends AppCompatActivity implements LocationListener,
         if (nCurrentSpeed > 0) {
             AvgSpeedList.add(nCurrentSpeed);
             Double average = AvgSpeedList.stream().mapToDouble(val -> val).average().orElse(0.0);
-            Formatter fmt3 = new Formatter(new StringBuilder());
-            fmt3.format(Locale.US, "%5.0f", average);
-            strAverage = fmt3.toString();
+            Formatter fmt4 = new Formatter(new StringBuilder());
+            fmt4.format(Locale.US, "%5.0f", average);
+            strAverage = fmt4.toString();
             TV_AvgSpeed.setText("Avg. speed: " + strAverage + " km/h");
         }
-
 
 
         if (this.useMetricUnits()) {
 
             TV_speed.setText(strCurrentSpeed + " \nkm/h");
-        }
-        else {
+        } else {
             TV_speed.setText(strCurrentSpeed + " \nmph");
         }
+    }
+
+    private void updateLean() {
+
+
+        LeanAngle = SensorManager.AXIS_X;
+
+        Formatter fmt3 = new Formatter(new StringBuilder());
+        fmt3.format(Locale.US, "%5.0f", LeanAngle);
+        String strLeanAngle = fmt3.toString();
+
+        TV_LeanAngle.setText("Avg. speed: " + strLeanAngle + " km/h");
+
+        Log.d("Main", "updateLean: " + LeanAngle);
     }
 
     private boolean useMetricUnits() {
@@ -234,12 +288,85 @@ public class MainActivity extends AppCompatActivity implements LocationListener,
         TV_TopSpeed.setText("Top speed: " + TopSpeed + " km/h");
 
 
-        if (strAverage == null){
+        if (strAverage == null) {
             TV_AvgSpeed.setText("Avg. speed: " + "0" + " km/h");
-        }
-        else {
+        } else {
             TV_AvgSpeed.setText("Avg. speed: " + strAverage + " km/h");
         }
 
+    }
+
+
+    public void onSensorChanged(SensorEvent event) {
+        if (event.sensor == mAccelerometer) {
+            System.arraycopy(event.values, 0, mLastAccelerometer, 0, event.values.length);
+            mLastAccelerometerSet = true;
+        } else if (event.sensor == mMagnetometer) {
+            System.arraycopy(event.values, 0, mLastMagnetometer, 0, event.values.length);
+            mLastMagnetometerSet = true;
+        }
+        if (mLastAccelerometerSet && mLastMagnetometerSet) {
+            SensorManager.getRotationMatrix(mR, null, mLastAccelerometer, mLastMagnetometer);
+            SensorManager.getOrientation(mR, mOrientation);
+            Log.i("OrientationTestActivity", String.format("Orientation: %f, %f, %f",
+                    mOrientation[0], mOrientation[1], mOrientation[2]));
+        }
+
+
+
+
+
+        Log.d("Main","updateLean: "+mOrientation[0]);
+
+//        int iLeanAngle = (int) (event.values[0] *9);
+        int iLeanAngle = (int) (mOrientation[0]);
+
+        // *57.2957768
+
+        TV_LeanAngle.setText("Lean Angle: "+iLeanAngle +"°");
+
+        if(iLeanAngle >MaxLeanL)
+        {
+            MaxLeanL = iLeanAngle;
+            TV_MaxLeanL.setText("L: " + iLeanAngle + "°");
+        }
+
+        if(iLeanAngle<MaxLeanR)
+        {
+            MaxLeanR = iLeanAngle;
+            TV_MaxLeanR.setText("R: " + iLeanAngle + "°");
+        }
+
+    //        Log.d("Main", "updateLean: " +event.values[0]);
+
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+    }
+
+//    @Override
+//    protected void onResume() {
+//        super.onResume();
+//        sensorManager.registerListener(this, sensor, SensorManager.SENSOR_DELAY_NORMAL);
+//    }
+//
+//    @Override
+//    protected void onPause() {
+//        super.onPause();
+//        sensorManager.unregisterListener(this);
+//    }
+    protected void onResume() {
+        super.onResume();
+        mLastAccelerometerSet = false;
+        mLastMagnetometerSet = false;
+        mSensorManager.registerListener(this, mAccelerometer, SensorManager.SENSOR_DELAY_NORMAL);
+        mSensorManager.registerListener(this, mMagnetometer, SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    protected void onPause() {
+        super.onPause();
+        mSensorManager.unregisterListener(this);
     }
 }
--- MotorcycleDashboard/app/src/main/java/com/example/motorcycledashboard/Map.java
+++ MotorcycleDashboard/app/src/main/java/com/example/motorcycledashboard/Map.java
@@ -1,5 +1,180 @@
 package com.example.motorcycledashboard;
 
-public class Map {
+import android.Manifest;
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.nfc.Tag;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.ActivityChooserView;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+
+import java.util.ArrayList;
+
+public class Map extends AppCompatActivity implements OnMapReadyCallback {
+
+    private static final String TAG = "Map";
+
+    private MarkerOptions options = new MarkerOptions();
+    private ArrayList<LatLng> latlngs = new ArrayList<>();
+
+    private static final String FINE_LOCATION = Manifest.permission.ACCESS_FINE_LOCATION;
+    private static final String COARSE_LOCATION = Manifest.permission.ACCESS_COARSE_LOCATION;
+
+    private boolean mLocPermissionGranted = false;
+    private static final int Loc_Permissions_code = 1234;
+    private GoogleMap mMap;
+
+    private static final float DEF_ZOOM = 15f;
+
+    private FusedLocationProviderClient mFusedLocationProviderClient;
+
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.mainmap);
+
+        getLocPermissions();
+
+    }
+
+    @Override
+    public void onMapReady(@NonNull GoogleMap googleMap) {
+        Toast.makeText(this, "Map loaded", Toast.LENGTH_SHORT).show();
+        mMap = googleMap;
+
+        if (mLocPermissionGranted) {
+            GetDeviceLoc();
+
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                return;
+            }
+            mMap.setMyLocationEnabled(true);
+            mMap.getUiSettings().setMyLocationButtonEnabled(false);
+        }
+
+//                Marker m1 = mMap.addMarker(new MarkerOptions()
+//                .position(new LatLng(53.353946, -6.252994))
+//                .anchor(0.5f, 0.5f)
+//                .title("Title1")
+//                .snippet("Snippet1")
+//        );
+//        m1.setTag(0);
+
+
+        for (LatLng point : MainActivity.myObjects) {
+            options.position(point);
+            options.title("someTitle");
+            options.snippet("someDesc");
+            mMap.addMarker(options);
+        }
+
+        Log.i(TAG, "onMapReady: " + MainActivity.myObjects.get(1) + "....");
+
+
+    }
+
+
+
+
+    private void GetDeviceLoc() {
+        mFusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
+
+        try {
+            if(mLocPermissionGranted) {
+                Task location = mFusedLocationProviderClient.getLastLocation();
+                location.addOnCompleteListener(new OnCompleteListener() {
+                    @Override
+                    public void onComplete(@NonNull Task task) {
+                        if(task.isSuccessful()){
+                            Log.d(TAG, "onComplete: location found");
+                            Location currLoc = (Location) task.getResult();
+
+
+                            moveCam(new LatLng(currLoc.getLatitude(), currLoc.getLongitude()), DEF_ZOOM);
+                        }
+                        else {
+                            Log.d(TAG, "onComplete: location not found");
+                            Toast.makeText(Map.this, "Location not found", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+            }
+
+        } catch (SecurityException e) {
+            Log.e(TAG, "GetDeviceLoc: SecurityException:" + e.getMessage());
+        }
+    }
+
+    private void moveCam(LatLng latlng, float zoom){
+        Log.d(TAG, "moveCam: moving the camera Lat:" +latlng.latitude +"Long:"+ latlng.longitude);
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latlng, zoom));
+    }
+
+    private void InitMap() {
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+
+        mapFragment.getMapAsync(Map.this);
+
+
+    }
+
+    private void getLocPermissions() {
+        String[] permissions = {Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION};
+        if(ContextCompat.checkSelfPermission(this.getApplicationContext(), FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            if(ContextCompat.checkSelfPermission(this.getApplicationContext(), COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+                mLocPermissionGranted = true;
+                InitMap();
+            }
+            else {
+                ActivityCompat.requestPermissions(this, permissions, Loc_Permissions_code);
+            }
+        }
+        else {
+            ActivityCompat.requestPermissions(this, permissions, Loc_Permissions_code);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        mLocPermissionGranted = false;
+
+        switch (requestCode) {
+            case Loc_Permissions_code:{
+                if(grantResults.length > 0){
+                    for(int i=0; i<grantResults.length; i++) {
+                        if(grantResults[i] != PackageManager.PERMISSION_GRANTED) {
+                            mLocPermissionGranted = false;
+                            return;
+                        }
+                    }
+                    mLocPermissionGranted = true;
+                    InitMap();
+
+                }
+            }
+        }
+    }
+
 
 }
